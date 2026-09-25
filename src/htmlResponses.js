ae7994abd876b119fbacc23175d291547eed7a9a
vni5104 <vickyn5104@gmail.com>
2026-09-25 14:44:36 -0400

    fixed css directory

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-const css = fs.readFileSync(`${__dirname}/../client/Style.css`);
+const css = fs.readFileSync(`${__dirname}/../client/style.css`);
 
 const getIndex = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/html' });
